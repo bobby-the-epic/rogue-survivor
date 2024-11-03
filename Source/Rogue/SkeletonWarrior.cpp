596264b38b434e1a393107dfaba2a62e4a3c1658
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-11-03 00:22:26 -0700

    Add comment

--- Source/Rogue/SkeletonWarrior.cpp
+++ Source/Rogue/SkeletonWarrior.cpp
@@ -5,6 +5,7 @@ ASkeletonWarrior::ASkeletonWarrior()
 {
     PrimaryActorTick.bCanEverTick = false;
 
+    // Initialize the components and attach them to the appropriate sockets in the mesh.
     WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>("WeaponMesh");
     WeaponMesh->SetupAttachment(GetMesh(), "handslot_r");
     ShieldMesh = CreateDefaultSubobject<UStaticMeshComponent>("ShieldMesh");
