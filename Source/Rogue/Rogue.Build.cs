ad820e53f718e76b12be8897190e1d23918c4be6
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-11-13 18:45:44 -0800

    Add Niagara module

--- Source/Rogue/Rogue.Build.cs
+++ Source/Rogue/Rogue.Build.cs
@@ -9,6 +9,7 @@ public class Rogue : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
-                                                            "EnhancedInput", "UMG", "AIModule", "NavigationSystem" });
+                                                            "EnhancedInput", "UMG", "AIModule", "NavigationSystem",
+                                                            "Niagara" });
     }
 }
