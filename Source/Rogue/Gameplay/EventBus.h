26e2820cf465fc2f24357e75854ab01cf8c4add6
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-11-13 20:48:24 -0800

    Add collectible pickup delegate

--- Source/Rogue/Gameplay/EventBus.h
+++ Source/Rogue/Gameplay/EventBus.h
@@ -13,11 +13,15 @@ class ROGUE_API UEventBus : public UObject
     UDELEGATE()
     DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPlayerDeathSignature);
 
+    UDELEGATE()
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCollectiblePickupSignature);
+
   public:
     UEventBus();
     static UEventBus* Get();
     FOnPlayerMovedSignature OnPlayerMovedDelegate;
     FOnPlayerDeathSignature OnPlayerDeathDelegate;
+    FOnCollectiblePickupSignature OnCollectiblePickupDelegate;
 
   private:
     static UEventBus* Instance;
