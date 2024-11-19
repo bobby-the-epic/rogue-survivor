c70fa32f5e9744621273e6453840d2902d72cb92
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-11-19 01:32:27 -0800

    Add bomb class and blueprint

---
+++ Source/Rogue/Gameplay/Bomb.cpp
@@ -0,0 +1,55 @@
+#include "Bomb.h"
+#include "CombatInterface.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "EventBus.h"
+#include "Kismet/GameplayStatics.h"
+#include "MainGameMode.h"
+#include "Rogue/Characters/SkeletonWarrior.h"
+
+ABomb::ABomb()
+{
+    PrimaryActorTick.bCanEverTick = false;
+    BombMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BombMesh"));
+    RootComponent = BombMesh;
+    BlastRadius = CreateDefaultSubobject<USphereComponent>(TEXT("BlastRadius"));
+    BlastRadius->SetupAttachment(BombMesh);
+}
+void ABomb::BeginPlay()
+{
+    Super::BeginPlay();
+    EventBus = Cast<AMainGameMode>(UGameplayStatics::GetGameMode(GetWorld()))->GetEventBus();
+    BombMesh->OnComponentBeginOverlap.AddDynamic(this, &ABomb::OnOverlap);
+    BombMesh->OnComponentHit.AddDynamic(this, &ABomb::OnHit);
+    // BombMesh->AddImpulse(FVector(1, 0, 1) * 10000);
+}
+void ABomb::EndPlay(EEndPlayReason::Type EndPlayReason)
+{
+    BombMesh->OnComponentBeginOverlap.RemoveDynamic(this, &ABomb::OnOverlap);
+    BombMesh->OnComponentHit.RemoveDynamic(this, &ABomb::OnHit);
+    Super::EndPlay(EndPlayReason);
+}
+void ABomb::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
+                  FVector NormalImpulse, const FHitResult& Hit)
+{
+    Explode();
+}
+void ABomb::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                      int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    Explode();
+}
+void ABomb::Explode()
+{
+    TArray<AActor*> HitActors;
+    BlastRadius->GetOverlappingActors(HitActors, ASkeletonWarrior::StaticClass());
+    for (AActor* HitActor : HitActors)
+    {
+        ICombatInterface* Actor = Cast<ICombatInterface>(HitActor);
+        if (Actor != nullptr)
+        {
+            Actor->TakeDamage(WeaponDamage);
+        }
+    }
+    Destroy();
+}
