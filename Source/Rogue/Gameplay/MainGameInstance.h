8d4b7f87a826598d07ab5ede13579e8c259efa0e
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-12-03 20:25:02 -0800

    Add MainGameInstance class

---
+++ Source/Rogue/Gameplay/MainGameInstance.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MainGameInstance.generated.h"
+
+UCLASS()
+class ROGUE_API UMainGameInstance : public UGameInstance
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditDefaultsOnly)
+    int32 TimerMinutes = 30;
+
+  public:
+    int32 GetTimerMinutes() const { return TimerMinutes; }
+};
